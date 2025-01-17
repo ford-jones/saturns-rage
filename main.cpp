1ef0eee773ba41ec36d85828b8d432c4f1c92b28
ford-jones <fordnicholasjones@gmail.com>
2025-01-17 23:50:55 +1300

    Update: Clean code and add comments

--- main.cpp
+++ main.cpp
@@ -22,16 +22,15 @@ const float collision_radius = 2.0;
 
 int rand_offset_y = 0;
 int rand_offset_z = 0;
-
 float rotation_x_last_tick = 0.0;
 float rotation_z_last_tick = 0.0;
 
+std::unique_ptr<Lazarus::AudioManager>  audio_manager    = std::make_unique<Lazarus::AudioManager>();
 std::unique_ptr<Lazarus::WindowManager> window           = nullptr;
 std::unique_ptr<Lazarus::CameraManager> camera_manager   = nullptr;
 std::unique_ptr<Lazarus::TextManager>   text_manager     = nullptr;
 std::unique_ptr<Lazarus::LightManager>  light_manager    = nullptr;
 std::unique_ptr<Lazarus::MeshManager>   mesh_manager     = nullptr;
-// std::unique_ptr<Lazarus::AudioManager>  audio_manager    = nullptr;
 std::unique_ptr<Lazarus::WorldFX>       world            = nullptr;
 
 Lazarus::Shader                         shader_program;
@@ -43,7 +42,7 @@ Lazarus::CameraManager::Camera          camera          = {};
 Lazarus::LightManager::Light            light           = {};
 Lazarus::WorldFX::SkyBox                skybox          = {};
 
-int healthText = 0;
+int health_text_index = 0;
 
 struct Asteroid
 {
@@ -61,46 +60,52 @@ struct Spaceship
     Lazarus::MeshManager::Mesh mesh;
 };
 
-std::vector<Asteroid>                   asteroids       = {};
+std::vector<Lazarus::AudioManager::Audio> samples = {};
+std::vector<Asteroid> asteroids = {};
 Spaceship spaceship = {};
 
 void generate_random_numbers()
 {
+    //  Retrieve a random number between -8 and +8
     rand_offset_y = (0 + (rand() % 16)) - 8;
     rand_offset_z = (0 + (rand() % 16)) - 8;
 };
 
 void init()
 {   
+    //  Engine settings
     globals.setEnforceImageSanity(true);
     globals.setMaxImageSize(500, 500);
-
     globals.setLaunchInFullscreen(true);
     globals.setCursorHidden(true);
 
+    //  Construct window
+    //  TODO: Fix the need to be init window prior to shader
     window = std::make_unique<Lazarus::WindowManager>("Saturns Rage");
 
+    //  Initialise resources
     window->initialise();
     event_manager.initialise();
-
+    audio_manager->initialise();
     shader = shader_program.initialiseShader();
+    window->loadConfig(shader);
 
-    text_manager = std::make_unique<Lazarus::TextManager>(shader);
+    //  Construct managers
+    text_manager     = std::make_unique<Lazarus::TextManager>(shader);
     light_manager    = std::make_unique<Lazarus::LightManager>(shader);
     camera_manager   = std::make_unique<Lazarus::CameraManager>(shader);
     mesh_manager     = std::make_unique<Lazarus::MeshManager>(shader);
     world            = std::make_unique<Lazarus::WorldFX>(shader);
-    // audio_manager = std::make_unique<Lazarus::AudioManager>();
 
-    // audio_manager->initialise();
+    //  Spaceship definition
+    spaceship.mesh = mesh_manager->create3DAsset("assets/mesh/shuttle.obj", "assets/material/shuttle.mtl");
+    spaceship.health = 100;
+    spaceship.shield = 0;
+    spaceship.x_rotation = 0.0;
+    transformer.translateMeshAsset(spaceship.mesh, -15.0, -1.0, 0.0);
+    transformer.rotateMeshAsset(spaceship.mesh, 0.0, 180.0, 0.0);
 
-    window->loadConfig(shader);
-
-    text_manager->extendFontStack("assets/fonts/clock.ttf", 50);
-
-    light   = light_manager->createLightSource(0.0, 0.0, 0.0, 1.0, 1.0, 1.0);
-    camera  = camera_manager->createPerspectiveCam(0.0, -0.2, 0.0, 1.0, 0.0, 0.0);
-
+    //  Asteroid(s) definition
     for(int i = 0; i < max_asteroids; i++)
     {
         Asteroid asteroid;
@@ -120,24 +125,34 @@ void init()
         asteroids.push_back(asteroid);
     };
 
-    spaceship.mesh = mesh_manager->create3DAsset("assets/mesh/shuttle.obj", "assets/material/shuttle.mtl");
-    spaceship.health = 100;
-    spaceship.shield = 0;
-    spaceship.x_rotation = 0.0;
-    
-    transformer.translateMeshAsset(spaceship.mesh, -15.0, -1.0, 0.0);
-    transformer.rotateMeshAsset(spaceship.mesh, 0.0, 180.0, 0.0);
+    //  Spacial environment
+    skybox  = world->createSkyBox("assets/skybox/right.png", "assets/skybox/left.png", "assets/skybox/bottom.png", "assets/skybox/top.png", "assets/skybox/front.png", "assets/skybox/back.png");
+    light   = light_manager->createLightSource(0.0, 0.0, 0.0, 1.0, 1.0, 1.0);
+    camera  = camera_manager->createPerspectiveCam(0.0, -0.2, 0.0, 1.0, 0.0, 0.0);
 
-    skybox = world->createSkyBox("assets/skybox/right.png", "assets/skybox/left.png", "assets/skybox/bottom.png", "assets/skybox/top.png", "assets/skybox/front.png", "assets/skybox/back.png");
+    //  HUD
+    text_manager->extendFontStack("assets/fonts/clock.ttf", 50);
+    health_text_index = text_manager->loadText(std::string("Ship health: ").append(std::to_string(spaceship.health)), 0, 0, 10, 1.0f, 0.0f, 0.0f);
 
-    std::string hp = std::string("Ship health: ").append(std::to_string(spaceship.health));
-    healthText = text_manager->loadText(hp, 0, 0, 10, 1.0f, 0.0f, 0.0f);
+    //  Load audio
+    samples.push_back(audio_manager->createAudio("assets/sound/crash1.mp3"));
+    for(int i = 0; i < samples.size(); i++)
+    {
+        audio_manager->loadAudio(samples[i]);
+        audio_manager->pauseAudio(samples[i]);
+    };
+};
+
+void load_environment()
+{
+    camera_manager->loadCamera(camera);
+    light_manager->loadLightSource(light);
+
+    world->drawSkyBox(skybox, camera);
 };
 
 void draw_assets()
 {
-    world->drawSkyBox(skybox, camera);
-
     mesh_manager->loadMesh(spaceship.mesh);
     mesh_manager->drawMesh(spaceship.mesh);
 
@@ -147,11 +162,13 @@ void draw_assets()
         mesh_manager->drawMesh(asteroids[i].mesh);
     };
 
-    text_manager->drawText(healthText);
+    //  Note: Draw text last to overlay
+    text_manager->drawText(health_text_index);
 };
 
-int check_collisions(Lazarus::MeshManager::Mesh a, Lazarus::MeshManager::Mesh b)
+inline int check_collisions(Lazarus::MeshManager::Mesh a, Lazarus::MeshManager::Mesh b)
 {
+        //  Find the distance between two points in a volume
         //  d = √((x2 – x1)² + (y2 – y1)² + (z2 – z1)²).
 
         float diff_x = (a.locationX - b.locationX);
@@ -181,14 +198,18 @@ void game_end()
 
 void move_spaceship()
 {    
+    //  Get monitor dimensions
     float center_x = static_cast<float>(globals.getDisplayWidth() / 2);
     float center_y = static_cast<float>(globals.getDisplayHeight() / 2);
 
+    //  Get mouse position
     int mouse_x = event_manager.mouseX;
     int mouse_y = event_manager.mouseY;
 
+    //  Store the ship's rotation from previous iteration
     rotation_x_last_tick = spaceship.x_rotation;
 
+    //  Move right
     if(mouse_x > (center_x) + mouse_sensitivity)
     {
         transformer.translateMeshAsset(spaceship.mesh, 0.0, 0.0, 0.1);   
@@ -197,6 +218,7 @@ void move_spaceship()
 
         spaceship.x_rotation += 0.2;
     }
+    //  Move left
     else if(mouse_x < (center_x) - mouse_sensitivity)
     {
         transformer.translateMeshAsset(spaceship.mesh, 0.0, 0.0, -0.1);
@@ -206,12 +228,14 @@ void move_spaceship()
         spaceship.x_rotation += -0.2;
     };
 
+    //  Move up
     if(mouse_y > (center_y) + mouse_sensitivity)
     {
         transformer.translateMeshAsset(spaceship.mesh, 0.0, -0.1, 0.0);
         transformer.rotateMeshAsset(spaceship.mesh, 0.0, 0.0, 0.1);
         window->snapCursor(center_x, center_y);
     }
+    //  Move down
     else if(mouse_y < (center_y) - mouse_sensitivity)
     {
         transformer.translateMeshAsset(spaceship.mesh, 0.0, 0.1, 0.0);
@@ -219,6 +243,8 @@ void move_spaceship()
         window->snapCursor(center_x, center_y);
     };
 
+    //  Check for update to the ships rotation since last iteration
+    //  If no change, level out the ship
     if(rotation_x_last_tick == spaceship.x_rotation)
     {
         transformer.rotateMeshAsset(spaceship.mesh, -spaceship.x_rotation, 0.0, 0.0);
@@ -232,12 +258,15 @@ void move_asteroids()
 {
     for(int i = 0; i < asteroids.size(); i++)
     {
+        //  Update asteroid position
         transformer.translateMeshAsset(asteroids[i].mesh, asteroids[i].movement_speed, 0.0, 0.0);
 
+        //  Reset asteroid position
         if(asteroids[i].mesh.locationX > 0.0)
         {
             asteroids[i].has_colided = false;
-            transformer.rotateMeshAsset(asteroids[i].mesh, 0.0, -asteroids[i].y_rotation, -asteroids[i].z_rotation); //  Invert sign of last rotate + translate to move back to origin
+            //  Invert sign of last rotate + translate to move back to origin
+            transformer.rotateMeshAsset(asteroids[i].mesh, 0.0, -asteroids[i].y_rotation, -asteroids[i].z_rotation);
             transformer.translateMeshAsset(asteroids[i].mesh, -60.0, static_cast<float>(-asteroids[i].y_spawn_offset), static_cast<float>(-asteroids[i].z_spawn_offset));
 
             asteroids[i].z_rotation = 0.0;
@@ -245,7 +274,8 @@ void move_asteroids()
 
             generate_random_numbers();
 
-            transformer.translateMeshAsset(asteroids[i].mesh, 0.0, static_cast<float>(rand_offset_y), static_cast<float>(rand_offset_z));                     //  Perform new rand translation / asteroid spawn and update
+            //  Move asteroid to random offset from center
+            transformer.translateMeshAsset(asteroids[i].mesh, 0.0, static_cast<float>(rand_offset_y), static_cast<float>(rand_offset_z));
             asteroids[i].z_spawn_offset = rand_offset_z;
             asteroids[i].y_spawn_offset = rand_offset_y;
         };
@@ -253,19 +283,25 @@ void move_asteroids()
         int collision = 0;
         collision = check_collisions(spaceship.mesh, asteroids[i].mesh);
 
+        //  Check collision result
+        //  Note: Use has_colided so as not to clock 50+ collisions in a frame
         if((collision == 1) && asteroids[i].has_colided == false)
         {
             asteroids[i].has_colided = true;
-            spaceship.health = spaceship.health - collision_damage;
 
+            //  Set crash1.mp3 back to begining and play
+            audio_manager->setPlaybackCursor(samples[0], 1);
+            audio_manager->playAudio(samples[0]);
+
+            //  Bounce asteroid off ship
             generate_random_numbers();
             asteroids[i].z_rotation = rand_offset_z * 3;
             asteroids[i].y_rotation = rand_offset_y * 3;
-
             transformer.rotateMeshAsset(asteroids[i].mesh, 0.0, asteroids[i].y_rotation, asteroids[i].z_rotation);
 
-            std::string hp = std::string("Ship health: ").append(std::to_string(spaceship.health));
-            text_manager->loadText(hp, 0, 0, 10, 1.0f, 0.0f, 0.0f, healthText);
+            //  Update ship health
+            spaceship.health = spaceship.health - collision_damage;
+            text_manager->loadText(std::string("Ship health: ").append(std::to_string(spaceship.health)), 0, 0, 10, 1.0f, 0.0f, 0.0f, health_text_index);
 
             if(spaceship.health <= 0)
             {
@@ -277,37 +313,33 @@ void move_asteroids()
 
 void move_background()
 {
-    transformer.rotateMeshAsset(skybox.cube, 0.0, -0.5, 0.0);
+    transformer.rotateMeshAsset(skybox.cube, 0.0, -0.2, 0.0);
 };
 
 int main()
 {
     init();
-
     window->open();
 
     while(window->isOpen)
     {
-        if(game_over)
-        {
-            game_end();
-        };
-
         event_manager.listen();
 
-        camera_manager->loadCamera(camera);
-        light_manager->loadLightSource(light);
-
-        bool player_is_moving = false;
-
+        //  Render scene
+        load_environment();
         draw_assets();
+
+        //  Do game mechanics
         move_spaceship();
         move_asteroids();
         move_background();
-
-        int status = globals.getExecutionState();
         
-        if(status != LAZARUS_OK || event_manager.keyCode == 88)
+        if
+        (
+            globals.getExecutionState() != LAZARUS_OK   || // If some error has surfaced from engine state
+            event_manager.keyCode == 88                 || // Or the user hits the 'X' key
+            game_over                                      // Or a win/lose condition has been met
+        )
         {
             game_end();
         }
